edb208340eb5ff1c96444bd9c5477a96ac039145
adiemmanuel10@gmail.com <adiemmanuel10@gmail.com>
2025-10-31 04:00:44 +0000

    Initial commit of files

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,9 +13,11 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
+    
     <div
       className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
     >
+      <h1 className="text-white">HELLO WORLD</h1>
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
