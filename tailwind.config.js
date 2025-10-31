d05823cfb121615f6441fd8ba9f4f6813b659570
adiemmanuel10@gmail.com <adiemmanuel10@gmail.com>
2025-10-31 03:55:51 +0000

    Initial commit of files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 module.exports = {
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -5,3 +6,12 @@ module.exports = {
   },
   plugins: [],
 };
+=======
+module.exports = {
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+>>>>>>> 7e8e26f433e7f25330ca335adc91cb8fbd080e8a
