79d0e33500ecc6aac82b80da3b610092bb35a08e
adiemmanuel10@gmail.com <adiemmanuel10@gmail.com>
2025-10-31 04:45:48 +0000

    updated the globals.css file with import Tailwind’s base, components, and utilities

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="./assets/next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
@@ -57,7 +57,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="./assets/vercel.svg"
               alt="Vercel logomark"
               width={16}
               height={16}
@@ -65,7 +65,7 @@ export default function Home() {
             Deploy Now
           </a>
           <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="https://nextjs.org/docs/pages/getting-started?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
